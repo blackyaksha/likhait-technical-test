da4d9efdb70cec33f96a3238fa9d0553544b6e03
Emman <101398596+blackyaksha@users.noreply.github.com>
2026-03-12 22:07:12 +0800

    Changed "created_at" to date to properly list the expenses based on the date

--- backend/app/controllers/api/expenses_controller.rb
+++ backend/app/controllers/api/expenses_controller.rb
@@ -1,6 +1,7 @@
 class Api::ExpensesController < ApplicationController
   def index
-    expenses = Expense.includes(:category).order(created_at: :desc)
+    # Replaces the created_at to date for filtering 
+    expenses = Expense.includes(:category).order(date: :desc)
 
     if params[:year].present? && params[:month].present?
       year = params[:year].to_i
@@ -9,7 +10,8 @@ class Api::ExpensesController < ApplicationController
       start_date = Date.new(year, month, 1)
       end_date = start_date.end_of_month
 
-      expenses = expenses.where(created_at: start_date.beginning_of_day..end_date.end_of_day)
+      # Changed from created_at to date for filtering
+      expenses = expenses.where(date: start_date..end_date)
     end
 
     render json: expenses.map { |expense| format_expense(expense) }
